d84232cf7ffda7bb5d46d073042afb384261158e
Leander <lvink3@gmail.com>
2024-05-31 15:32:05 +0200

    Fixed deploy script - added dotenv

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,7 @@
 const hre = require("hardhat");
+const dotenv = require("dotenv")
+
+dotenv.config()
 
 async function main() {
   const fudToken = await hre.ethers.deployContract("Fud");
@@ -10,9 +13,7 @@ async function main() {
   const airVault = await hre.ethers.deployContract("AirVault", [fudToken.target]);
   await airVault.waitForDeployment();
 
-  await winToken.setMinter('0xa1f9A2E3431e57014c8794b4cdc8718A657bB3Ef', true)
-  await fudToken.transfer('0x56CE632320B8aF872b9ef5b94C4a483A63f1EE26', hre.ethers.parseEther('100'))
-  await fudToken.transfer('0x8d8A668F39Df525045cdeAE09F87C9430D1C20E8', hre.ethers.parseEther('100'))
+  await winToken.setMinter(process.env.MINTER_ADDRESS, true)
 
   console.log({
     fud: fudToken.target,
